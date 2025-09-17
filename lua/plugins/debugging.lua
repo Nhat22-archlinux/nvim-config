7ebfcca53a254fa0f0f1392bfe35c451939fbd91
ChouThun <testlinux.ubuntu22@gmail.com>
2025-09-17 22:26:43 +0700

    update nvim config (rainbow indent, lsp, treesitter, etc)

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -12,8 +12,8 @@ return {
 
     dap.listeners.before.attach.dapui_config = function() dapui.open() end
     dap.listeners.before.launch.dapui_config = function() dapui.open() end
-    dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-    dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+    -- dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+    -- dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
 
     -- Adapter pwa-node
     dap.adapters["pwa-node"] = {
@@ -41,6 +41,7 @@ return {
     -- Keymaps
     vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
     vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" }) -- 👈 thêm keymap đóng/mở bằng tay
   end,
 }
 
